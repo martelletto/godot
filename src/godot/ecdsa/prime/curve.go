5f5b2b7d3d55ee85ecdb66e6ff48d611da54411a
pedro martelletto <pedro@ambientworks.net>
2017-11-11 07:55:09 +0000

    adjust comment

--- src/godot/ecdsa/prime/curve.go
+++ src/godot/ecdsa/prime/curve.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file.
 //
-// curve.go elliptic curve arithmetic over prime curves.
+// curve.go implements elliptic curve arithmetic over prime
+// fields.
 
 package prime
 
